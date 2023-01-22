2203875a3eb91d54ba6f453c23cb7418b8a4193a
Tasauwar <tansari.2005@gmail.com>
2023-01-22 10:56:34 +0530

    first changes

---
+++ Webapppipelintest/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Webapppipelintest.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
